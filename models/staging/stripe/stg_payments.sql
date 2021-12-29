4120517b8e17c4c13914f6c44211606e860cfae4
Jordan Hartfiel <jordan.hartfiel@cityblock.com>
2021-12-29 21:03:39 +0000

    update-models

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,9 @@
+with payments as (
+select orderid as order_id,
+paymentmethod as payment_method,
+status,
+amount/100 as amount,
+created as created_at
+from `dbt-tutorial.stripe.payment`)
+
+select * from payments
